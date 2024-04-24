864648798d24b9f75367ca9d18840d138e866ab5
SRElena <313n4161@gmail.com>
2024-04-24 14:02:20 -0700

    changes were added to App.jsx and it was deleted index.css and App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import SearchBar from './components/Searchbar'
+import imageList from './components/imagenList'
+import SearchBar from './api'
 import './App.css'
+import searchImage from './api'
 
 function App() {
   const [images, setCount] = useState([])
+
+  const handleSubmit = async(term) =>{
+    console.log('Usted esta buscando con: ',term)
+    const result = await searchImage(term)
+
+    console.log('coco')
+    console.log(result)
+
+    setImages(result)
+  }
 
   return (
       <div>
         <h1>App</h1>
         <SearchBar onSumit={handleSubmit} />
+        <imageList images={images} />
     </div>
   )
 }
